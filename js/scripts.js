11d7d27d6bf555eeb8ab53b5d0708825bce606a0
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2020-03-19 14:36:56 -0700

    template and favicons

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+Splitting({
+target:"nav h1",
+by:"chars"
+})
+
+console.log("ok");
+
+gsap.timeline()
+
+lightGallery( document.querySelector('.gallery'),{
+    mode:'lg-fade',
+    backDropDuration: 10
+    }  );
+    gsap.timeline()
+    .from(".me",{
+        y:-10000, duration:2
+    })
+    .from("#right",{
+        x:10000,duration:2
+    })
+    .from("#left",{
+        x:-10000,duration:2,
+    })
+    .to("#form",{
+        opacity:0
+    })
+    .to(".word span:nth-child(even)",{
+        opacity:1, duration:1})
+    .to(".word span:nth-child(odd)",{
+      opacity:1,duration:1});
+      
+
+    
